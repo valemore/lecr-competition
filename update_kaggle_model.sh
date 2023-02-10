841b2f6e6a3911b2671d2d6947faed401a9db253
Valerio Morelli <44409903+valemore@users.noreply.github.com>
2023-02-10 18:28:10 +0100

    kaggle scripts

--- update_kaggle_model.sh
+++ update_kaggle_model.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 FNAME=$1
 cp ../out/FNAME ../kaggle/model/biencoder.pt
-kaggle datasets version --dir-mode "zip" -p  ../kaggle/model -m "Update biencoder $FNAME"
+kaggle datasets version -p  ../kaggle/model -m "Update biencoder $FNAME"
--- update_kaggle_src.sh
+++ update_kaggle_src.sh
@@ -2,4 +2,4 @@
 GITHASH=`git rev-parse --short HEAD`
 rsync --exclude-from exclude_file.txt -r . ../kaggle/code/src
 touch ../kaggle/code/src/dummy
-# kaggle datasets version --dir-mode "zip" -p  ../kaggle/code -m $GITHASH
+kaggle datasets version --dir-mode "zip" -p  ../kaggle/code -m $GITHASH
