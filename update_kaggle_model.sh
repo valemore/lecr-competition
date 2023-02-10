34a988a9cee04c781df9dfccd8f06915b4b9910d
Valerio Morelli <44409903+valemore@users.noreply.github.com>
2023-02-11 00:05:49 +0100

    fix update_kaggle_model.sh

--- update_kaggle_model.sh
+++ update_kaggle_model.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 FNAME=$1
-cp -r ../out/FNAME ../kaggle/model/
+cp -r ../out/$FNAME/* ../kaggle/model/
 kaggle datasets version --dir-mode "zip" -p  ../kaggle/model -m "Update biencoder $FNAME"
