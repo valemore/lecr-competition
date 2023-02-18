b8362516cf8aa55084c672cb0148d719663f9a4d
Valerio Morelli <44409903+valemore@users.noreply.github.com>
2023-02-18 22:53:02 +0100

    fix setup_vastai.sh

--- setup_vastai.sh
+++ setup_vastai.sh
@@ -2,13 +2,13 @@
 SSH_PROFILE=$1
 ssh $SSH_PROFILE 'mkdir -p .kaggle'
 scp ~/.kaggle/kaggle.json $SSH_PROFILE:~/.kaggle/
-ssh $SSH_PROFILE 'mkdir kolibri'
+ssh $SSH_PROFILE 'mkdir -p kolibri'
 rsync --exclude-from exclude_file.txt -r ~/v/kolibri/kolibri-code/ $SSH_PROFILE:~/kolibri/kolibri-code
 
 scp ~/.ssh/vastai $SSH_PROFILE:~/.ssh/
 scp ~/.ssh/vastai.pub $SSH_PROFILE:~/.ssh/
 
-ssh vastai1 'tmux new -d -s sesh'
-ssh vastai1 'tmux send-keys -t sesh.0 "cd ~/kolibri/kolibri-code && bash setup_env.sh" ENTER'
-ssh vastai1 'tmux send-keys -t sesh.0 "cd ~/kolibri/kolibri-code && bash vastai_cmds.sh" ENTER'
-ssh vastai1 'tmux kill-session -t sesh'
+ssh $SSH_PROFILE 'tmux new -d -s sesh'
+ssh $SSH_PROFILE 'tmux send-keys -t sesh.0 "cd ~/kolibri/kolibri-code && bash setup_env.sh" ENTER'
+ssh $SSH_PROFILE 'tmux send-keys -t sesh.0 "cd ~/kolibri/kolibri-code && bash vastai_cmds.sh" ENTER'
+ssh $SSH_PROFILE 'tmux kill-session -t sesh'
