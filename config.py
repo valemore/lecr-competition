8492b16d198372a03bd51d5c0282d7234fa71bfe
Valerio Morelli <44409903+valemore@users.noreply.github.com>
2023-02-22 22:59:08 +0100

    use 400 neighbors

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ class CFG:
     CONTENT_NUM_TOKENS = 128
     SCORE_FN = cos_sim
 
-    NUM_NEIGHBORS = 100
+    NUM_NEIGHBORS = 400
 
     # Cross-Encoder
     CROSS_MODEL_NAME = "bert-base-multilingual-uncased"
