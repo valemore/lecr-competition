ce2917d8ff8c0722cb7aaa5e3c7f6bde6dd05102
Valerio Morelli <44409903+valemore@users.noreply.github.com>
2023-02-23 00:34:56 +0100

    cross submission and inference missing file

---
+++ cross/inference.py
@@ -0,0 +1,22 @@
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from config import CFG
+from cross.dset import CrossInferenceDataset
+
+
+def predict(model, dset: CrossInferenceDataset, thresh: float, batch_size: int, device: torch.device):
+    loader = DataLoader(dset, batch_size, num_workers=CFG.NUM_WORKERS, shuffle=False)
+    all_preds = []
+    model.eval()
+    for batch in tqdm(loader):
+        batch = tuple(x.to(device) for x in batch)
+        with torch.no_grad():
+            out = model(*batch)
+        logits = out.logits
+        probs = logits.softmax(dim=1)[:, 1]
+        all_preds.append((probs >= thresh).cpu().numpy())
+    all_preds = np.concatenate(all_preds, axis=0).astype(int)
+    return all_preds
