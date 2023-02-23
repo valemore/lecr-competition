04280d1decd49216d1b2330f4282d1bf02e5167a
Valerio Morelli <44409903+valemore@users.noreply.github.com>
2023-02-23 23:05:06 +0100

    fix update_kaggle_cross.sh

--- update_kaggle_cross.sh
+++ update_kaggle_cross.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 FNAME=$1
-cp -r ../out/$FNAME/* ../kaggle/model/
+cp -r ../out/$FNAME/* ../kaggle/cross/
 kaggle datasets version --dir-mode "zip" -p  ../kaggle/cross -m "Update crossencoder $FNAME"
