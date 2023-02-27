6b6c5d368f2921dc5087b1f4261b48f2791a79ab
Valerio Morelli <44409903+valemore@users.noreply.github.com>
2023-02-27 13:17:59 +0100

    small update to update_kaggle_src.sh

--- update_kaggle_src.sh
+++ update_kaggle_src.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 GITHASH=`git rev-parse --short HEAD`
-rsync --exclude-from exclude_file.txt -r . ../kaggle/code/src
+rsync --exclude-from exclude_file.txt -r --delete . ../kaggle/code/src
 touch ../kaggle/code/src/dummy
 kaggle datasets version --dir-mode "zip" -p  ../kaggle/code -m $GITHASH
