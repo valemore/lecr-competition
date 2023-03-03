2b5e3551a4a2b107696ed7f719033921e3dab38b
Valerio Morelli <44409903+valemore@users.noreply.github.com>
2023-03-03 22:54:24 +0100

    update_kaggle_cross.sh always generates dummy

--- update_kaggle_cross.sh
+++ update_kaggle_cross.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 FNAME=$1
 cp -r ../cout/$FNAME/* ../kaggle/cross/
+touch ../kaggle/cross/dummy
 kaggle datasets version --dir-mode "zip" -p  ../kaggle/cross -m "Update crossencoder $FNAME"
