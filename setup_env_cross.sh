73a1ec3e23b8c4ec27b57bb2daa3c4ef64a98165
Valerio Morelli <44409903+valemore@users.noreply.github.com>
2023-03-11 13:07:43 +0100

    add commented out pip line for deberta (sentencepiece and protobuf)

--- setup_env_cross.sh
+++ setup_env_cross.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 conda install -y pandas scikit-learn
 pip install neptune-client==0.16.17 transformers==4.26.0
+# pip install neptune-client==0.16.17 transformers==4.26.0 sentencepiece==0.1.97 protobuf==3.20
